d75eb39b438a871aec5de948a9e16e306e4eee7c
mayorgamaximo <135658035+mayorgamaximo@users.noreply.github.com>
2025-05-14 21:13:20 -0300

    comentarios

--- script.js
+++ script.js
@@ -21,15 +21,15 @@ function calculate(value) {
       result.value = "Error";
     }
   } else if (value === "Math.sqrt()") {
-    result.value = Math.sqrt(result.value); //hace la raiz cuadrada del numero ingresado
+    result.value = Math.sqrt(result.value); //hace la raiz cuadrada del numero ingresado y le da el valor para mostrar en el display
   } else if (value === "Math.sin(") {
-    result.value = eval(Math.sin(result.value)); //hace el seno del numero ingresado
+    result.value = eval(Math.sin(result.value)); //hace el seno del numero ingresado y le da el valor para mostrar en el display
   } else if (value === "Math.cos(") {
-    result.value = eval(Math.cos(result.value)); //hace el coseno del numero ingresado
+    result.value = eval(Math.cos(result.value)); //hace el coseno del numero ingresado y le da el valor para mostrar en el display
   } else if (value === "Math.tan(") {
-    result.value = eval(Math.tan(result.value)); //hace la tangente del numero ingresado
+    result.value = eval(Math.tan(result.value)); //hace la tangente del numero ingresado y le da el valor para mostrar en el display
   } else if (value === "Math.log(") {
-    result.value = eval(Math.log(result.value)); //hace el logaritmo del numero ingresado
+    result.value = eval(Math.log(result.value)); //hace el logaritmo del numero ingresado y le da el valor para mostrar en el display
   } else {
     result.value += value; // D) Añade el valor del botón al input
   }
